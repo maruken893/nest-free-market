87e9b4933af5f8e967645d63c04d97d7d2e2c62d
maruken893 <kentarooooo555@gmail.com>
2023-05-06 18:07:27 +0900

    Item登録機能を追加

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
+
   findAll() {
     return 'This findAll method called from items service';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
